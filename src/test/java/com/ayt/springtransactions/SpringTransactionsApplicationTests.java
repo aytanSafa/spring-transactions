8d15fb6da36bc7c5e01c7df20faaa1ca2246abe9
safaa <aytansafa@gmail.com>
2021-05-24 17:43:00 +0300

    Project starter commit

---
+++ src/test/java/com/ayt/springtransactions/SpringTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayt.springtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
