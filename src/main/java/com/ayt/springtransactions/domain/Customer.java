fde2812c14fd9fca2849fafa727edcf2cb55abc6
safaa <aytansafa@gmail.com>
2021-05-24 18:14:27 +0300

    Domain package added and defined H2 db configuration

---
+++ src/main/java/com/ayt/springtransactions/domain/Customer.java
@@ -0,0 +1,45 @@
+package com.ayt.springtransactions.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column (name = "surname")
+    private String surname;
+
+    @Column (name = "age")
+    private int age;
+
+    @OneToMany(
+            mappedBy = "customer",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Orders> OrderList;
+
+
+
+
+
+
+}
---
+++ src/main/java/com/ayt/springtransactions/domain/Orders.java
@@ -0,0 +1,36 @@
+package com.ayt.springtransactions.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "order_date")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private Date orderDate;
+
+    @Column(name = "order_total")
+    private double orderTotal;
+
+    @JoinColumn(name = "customer_id")
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    private Customer customer;
+
+}
